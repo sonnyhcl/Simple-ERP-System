941b007cee76d55533aab0fb5c731c177761bf68
sonnyhcl <sonnyhcl@163.com>
2017-04-10 22:28:07 +0800

    上传数据库设计表和url设计表

--- webapp/views.py
+++ webapp/views.py
@@ -1,14 +1,19 @@
 from webapp import app
-from flask import render_template
+from flask import render_template, request
 __author__ = 'sonnyhcl'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
