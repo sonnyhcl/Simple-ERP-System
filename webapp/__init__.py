29933b42169addbc20127a1076bfea55e5f73a4b
sonnyhcl <sonnyhcl@163.com>
2017-04-20 22:52:32 +0800

    fix

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,18 +1,12 @@
 # -*- coding: UTF-8 -*-
 import os
 from flask import Flask
-import logging
-from logging.handlers import SysLogHandler
-app = Flask(__name__)
-from mylog import log
 from config import config
-from webapp import views
-from webapp import auth
-from webapp import db
-
+app = Flask(__name__)
+app.config.from_object(config[os.getenv('FLASK_CONFIG') or 'default'])
 __author__ = 'sonnyhcl'
 
-app.config.from_object(config[os.getenv('FLASK_CONFIG') or 'default'])
+import logging
 log_path = os.path.join(app.config['PROJECT_PATH'], 'logs')
 if not os.path.exists(log_path):
     os.mkdir(log_path)
@@ -21,6 +15,12 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(message)s',
                     filename=log_file,
                     filemode='a')
+from logging.handlers import SysLogHandler
 syslog_handler = SysLogHandler()
 syslog_handler.setLevel(logging.DEBUG)
 app.logger.addHandler(syslog_handler)
+
+from mylog import log
+from webapp import views
+from webapp import auth
+from webapp import db
--- webapp/db/__init__.py
+++ webapp/db/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: UTF-8 -*-
 from db import db_user
 from db import db_engine
-from .db_engine import MySQL
-
 __author__ = 'sonnyhcl'
