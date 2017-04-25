ee44ee00b88f84f863bb59ac34efbd5aa211eee5
sonnyhcl <sonnyhcl@163.com>
2017-04-25 12:42:34 +0800

    增加任务的后台数据库API

--- webapp/views/mission.py
+++ webapp/views/mission.py
@@ -9,3 +9,26 @@ __author__ = 'sonnyhcl'
 @app.route('/mission', methods=['GET'])
 def mission_index():
     return render_template('mission.html')
+
+
+
+@app.route('/mission/table', methods=['POST'])
+def get_mission_by_cid():
+    """
+    返回社区c_id的任务信息
+    c_id=0意味着返回所有任务信息
+    :return: 
+    """
+    ret = {"data": [], "status": 'Success', "msg": ""}
+    # TODO status, info = mission.get_mission_by_cid()
+    ret['data'] = [
+        {'o_id': 1, 'p_name': "产品A", 'i_name': '产品A的工艺1',
+         'u_name': 'hcl', 'm_amount': 10, 'm_note': '无'},
+        {'o_id': 2, 'p_name': "产品A", 'i_name': '产品A的工艺2',
+         'u_name': 'hcl', 'm_amount': 20, 'm_note': '无'},
+        {'o_id': 3, 'p_name': "产品B", 'i_name': '产品B的工艺1',
+         'u_name': 'hcl', 'm_amount': 30, 'm_note': '无'},
+        {'o_id': 4, 'p_name': "产品B", 'i_name': '产品B的工艺2',
+         'u_name': 'hcl', 'm_amount': 40, 'm_note': '无'},
+    ]
+    return json.dumps(ret)
