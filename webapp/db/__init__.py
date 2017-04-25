362bf62fe3cfc80a6117074d32c7462488e65dac
sonnyhcl <sonnyhcl@163.com>
2017-04-25 10:32:26 +0800

    修改首页显示的用户信息

--- webapp/db/__init__.py
+++ webapp/db/__init__.py
@@ -1,5 +1,2 @@
 # -*- coding: UTF-8 -*-
-from db import db_user
-from db import db_community
-
 __author__ = 'sonnyhcl'
--- webapp/db/db_index.py
+++ webapp/db/db_index.py
@@ -1,5 +1,33 @@
 # -*- coding: UTF-8 -*-
-__author__ = 'sonnyhcl'
 """
 首页所有数据库相关的操作
 """
+
+import sqlite3
+from flask import session
+from webapp.mylog import log
+__author__ = 'sonnyhcl'
+
+
+class Index(object):
+
+    def get_index_info_by_uid(self, u_id):
+        """
+        在首页上需要显示username, user_role, community_name, user_phone, 
+        :return: 
+        """
+        # TODO 返回格式status, msg = ..... 以及异常处理
+        log("%s: get_index_info" % (session['u_name']))
+        conn = sqlite3.connect("test.db")
+        param = (u_id, )
+        response = conn.execute('SELECT u_name, u_role, c_name, u_phone '
+                                'FROM user, community '
+                                'WHERE user.u_id = ? '
+                                'and community.c_id = user.c_id',
+                                param)
+        response = response.fetchall()
+        conn.commit()
+        conn.close()
+        return "Success", response
+
+index = Index()
