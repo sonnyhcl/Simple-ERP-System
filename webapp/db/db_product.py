336ce5f9a954beefda418514c3760af932958d2e
tinylic <13307130163@fudan.edu.cn>
2017-04-27 00:16:19 +0800

    modified product for exception

--- webapp/db/db_product.py
+++ webapp/db/db_product.py
@@ -23,10 +23,19 @@ class Product(object):
 
     def __init__(self) :
         self.__counter = 0
-    def add_product(self, p_name, author_name = ""):
+    def add_product(self, p_name, author_name = "", i_name, i_unit_price, i_ref_time):
         conn = sqlite3.connect("demo.db");
-        param = (None, p_name, author_name,)
-        conn.execute('insert into product values (?, ?, ?);', param)
+        try :
+            param = (p_name, author_name,)
+            conn.execute('insert into product values (p_name, author_name);', param)
+            response = conn.execute('select p_id from product where p_name = ? and author_name = ?;', param)
+            response = response.fetchall()
+            p_id = response[0][0]
+            param = (i_name, i_unit_price, i_ref_time, p_id,)
+            conn.execute('insert into item values (i_name = ?, i_unit_price = ?, i_ref_time = ?, p_id = ?);', param)
+        except Exception:
+            conn.close()
+            return "Fail", Exception
         conn.commit()
         conn.close()
         return "Success"
@@ -35,24 +44,44 @@ class Product(object):
         conn = sqlite3.connect("demo.db");
 
         param = (p_id,)
-        conn.execute('delete from product where p_id = ?;', param)
+        try :
+            conn.execute('delete from item where p_id = ?;', param)
+            conn.execute('delete from product where p_id = ?;', param)
+        except Exception:
+            conn.close()
+            return "Fail", Exception
         conn.commit()
         conn.close()
         return "Success"
 
-    def update_product(self, p_id, p_name = None, author_name = None):
+    def update_product(self, p_id, p_name = None, author_name = None, i_id, i_name = None, i_unit_price = None, i_ref_time = None):
         conn = sqlite3.connect("demo.db");
-
-        param = (p_id,)
-        response = conn.execute('select * from product where p_id = ?;', param)
-        origin = response.fetchall()[0]
-        origin = list(origin)
-        if p_name          is not None :
-            origin[1] = p_name
-        if author_name     is not None :
-            origin[2] = author_name
-        param = tuple(origin) + (p_id,)
-        conn.execute('update product set p_id = ?, p_name = ?, author_name = ? where p_id = ?;', param)
+        try:
+            param = (p_id,)
+            response = conn.execute('select * from product where p_id = ?;', param)
+            origin = response.fetchall()[0]
+            origin = list(origin)
+            if p_name          is not None :
+                origin[1] = p_name
+            if author_name     is not None :
+                origin[2] = author_name
+            param = tuple(origin) + (p_id,)
+            conn.execute('update product set p_id = ?, p_name = ?, author_name = ? where p_id = ?;', param)
+            param = (i_id,)
+            response = conn.execute('select * from item where i_id = ?;', param)
+            origin = response.fetchall()[0]
+            origin = list(origin)
+            if i_name           is not None :
+                origin[1] = i_name
+            if i_unit_price     is not None :
+                origin[2] = i_unit_price
+            if i_ref_time       is not None:
+                origin[3] = i_ref_time
+            param = tuple(origin) + (i_id,)
+            conn.execute('update item set i_id = ?, i_name = ?, i_unit_price = ?, i_ref_time = ? where i_id = ?;', param)
+        except Exception:
+            conn.close()
+            return "Fail", Exception
         conn.commit()
         conn.close()
         return "Success"
