0e1e975da231740353faf5ba1909a1f9923d7081
sonnyhcl <sonnyhcl@163.com>
2017-04-28 17:13:47 +0800

    update log usage

--- webapp/mylog.py
+++ webapp/mylog.py
@@ -1,7 +1,10 @@
 # -*- coding: UTF-8 -*-
+from time import asctime
+from flask import session
 from webapp import app
 
 
 def log(msg):
     if type(msg) == str or type(msg) == unicode:
-        app.logger.info("[Operations] " + msg)
+        app.logger.info("[%s]%s: %s" %
+                        (asctime(), session['u_name'], msg))
