01adf9451adb17181bf5b9e9db627335557d845f
tinylic <lic.tiny@gmail.com>
2017-04-28 13:43:36 +0800

    modified mission and transactions

--- webapp/db/db_mission.py
+++ webapp/db/db_mission.py
@@ -9,12 +9,12 @@ from flask import session
 from webapp.mylog import log
 
 class Mission(object) :
-    def add_mission(self, u_name, i_name, m_amount):
+    def add_mission(self, u_id, i_id, o_id, m_amount):
         conn = sqlite3.connect("demo.db")
-        param = (o_amount, o_money, p_id,)
+        param = (u_id, i_id, o_id, m_amount,)
         try:
-            conn.execute('INSERT INTO order(o_amount, o_money, p_id)'
-                         'VALUES (?, ?, ?);', param)
+            conn.execute('INSERT INTO mission(u_id, i_id, o_id, m_amount)'
+                         'VALUES (?, ?, ?, ?);', param)
         except Exception:
             conn.close()
             return "Fail", traceback.print_exc()
@@ -22,11 +22,11 @@ class Mission(object) :
         conn.close()
         return "Success", ""
 
-    def delete_mission(self, o_id):
+    def delete_mission(self, m_id):
         conn = sqlite3.connect("demo.db")
-        param = (o_id,)
+        param = (m_id,)
         try:
-            conn.execute('DELETE FROM order WHERE o_id = ?;', param)
+            conn.execute('DELETE FROM mission WHERE m_id = ?;', param)
         except Exception:
             conn.close()
             return "Fail", traceback.print_exc()
@@ -34,8 +34,36 @@ class Mission(object) :
         conn.close()
         return "Success", ""
 
-    def update_mission(self, o_id, p_id, c_id, amount):
-        pass
+    def update_mission(self, m_id, amount = None, notes = None, u_id = None, i_id = None, o_id = None):
+        conn = sqlite3.connect("demo.db")
+        param = (m_id,)
+        try:
+            response = conn.execute('SELECT * FROM mission WHERE m_id = ?;', param)
+            origin = response.fetchall()[0]
+            origin = list(origin)
+            if amount is not None:
+                origin[1] = amount
+            if notes is not None:
+                origin[2] = notes
+            if u_id is not None:
+                origin[3] = u_id
+            if i_id is not None:
+                origin[4] = i_id
+            if o_id is not None:
+                origin[5] = o_id
+
+            param = tuple(origin) + (m_id,)
+            conn.execute(
+                'UPDATE mission '
+                'SET m_id = ?, amount = ?, m_notes = ?, u_id = ?, i_id = ?, o_id = ?'
+                'WHERE m_id = ?;',
+                param)
+        except Exception:
+            conn.close()
+            return "Fail", traceback.print_exc()
+        conn.commit()
+        conn.close()
+        return "Success", ""
 
     def get_mission_by_cid(self, c_id):
         conn = sqlite3.connect("demo.db")
