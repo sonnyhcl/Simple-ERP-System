de4cb5bfeab4a7a232333c9c16ee1314804d2f8f
sonnyhcl <sonnyhcl@163.com>
2017-05-03 08:24:13 +0800

    同上

--- webapp/views/transaction.py
+++ webapp/views/transaction.py
@@ -14,33 +14,6 @@ def transaction_index():
     return render_template('transaction.html')
 
 
-@app.route('/transaction/tableeee', methods=['POST'])
-@login_required
-def get_transaction_by_uid():
-    """
-    显示该uid人员的所有流水信息
-    :return: 
-    """
-    ret = {"data": [], "status": 'Success', "msg": ""}
-
-    u_id = request.form.get('u_id')
-    status, info = transaction.get_transaction_by_uid(u_id)
-    if status == "Success":
-        _ = [ret['data'].append(
-            {'t_id': i[0], 't_amount': i[1], 't_timestamp': i[2],
-             't_note': i[3], 'm_id': i[5], 'm_amount': i[6], 'm_note': i[7],
-             'o_id': i[11], 'o_amount': i[12], 'o_money': i[13],
-             'o_timestamp': i[14], 'o_note': i[15], 'p_id': i[16],
-             'p_name': i[19], 'p_author': i[20], 'u_name': i[22],
-             'u_role': i[23], 'c_name': i[28], 'i_name': i[31], 'i_note': i[34],
-             'i_unit_price': i[32]
-             }) for i in info]
-    else:
-        ret['msg'] = info
-
-    return json.dumps(ret, ensure_ascii=False)
-
-
 @app.route('/transaction/table', methods=['POST'])
 def get_transaction_table():
     """
